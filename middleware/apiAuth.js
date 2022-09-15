c9586ffbabad7a64ee2849d3729ce14cdab19b4e
Taner Akdemir <admin@tsmsoft.net>
2022-09-15 22:01:01 +0300

    apit auth is improved

--- middleware/apiAuth.js
+++ middleware/apiAuth.js
@@ -1,10 +1,29 @@
+const query = require("../mysql");
+const jwt = require("jsonwebtoken")
+
 module.exports = (req, res, next) => {
   try {
     const {authorization} = req.headers
-    if (authorization !== "abde123*trgdf") {
+    if (!authorization) {
       return res.status(403).json({success: false, message: "You are not authorised"})
     }
-    next()
+
+    jwt.verify(authorization, process.env.JWT_SECRET_KEY, {}, async (error, payload) => {
+      if (error) {
+        return res.status(403).json({success: false, message: "You are not authorised"})
+      }
+
+      const {id, email} = payload
+
+      const selectQuery = `select id, email, name from user where email='${email}' and id=${id}`
+      let isUserExist = await query(selectQuery)
+      if (isUserExist.length === 0) {
+        return res.status(403).json({success: false, message: "You are not authorised"})
+      }
+      req.user = isUserExist[0]
+      next()
+    })
+
   } catch (e) {
     return res.status(500).json({success: false, message: "Oppps.. An error occurred. " + e.message})
   }
--- pages/users.js
+++ pages/users.js
@@ -28,16 +28,16 @@ app.get('/:userId(\\d+)', apiAuth, (req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", "application/json")
   //console.log(module)
-  return res.json({message: "Success user detail"})
+  return res.json({user: req.user, message: "Success user detail"})
 })
 
-app.post('/', apiAuth, (req, res) => {
+/*app.post('/', apiAuth, (req, res) => {
   console.log(req.query)
   console.log(req.body)
   res.statusCode = 200
   res.setHeader("Content-Type", "application/json")
   return res.json({message: "Success user post"})
-})
+})*/
 
 
 app.post('/generate-token', async (req, res) => {
@@ -53,28 +53,68 @@ app.post('/generate-token', async (req, res) => {
       return res.status(403).json({message: "Password is not valid"})
     }
 
-    const result = await query(`select * from user where email='${email}' and password='${password}'`)
-
-    if (result.length === 0) {
-      //return res.status(403).json({message: "No user found"})
-    }
-
-    jwt.sign({
-      id: 1,
-      email,
-    }, process.env.JWT_SECRET_KEY, {expiresIn: '1d'}, (error, token) => {
+    jwt.sign(password, process.env.JWT_SECRET_KEY, {}, async (error, encryptedPassword) => {
       if (error) {
         return res.json({message: "Error when creating your token"})
       } else {
-        return res.json({token, message: "Success"})
+        const result = await query(`select * from user where email='${email}' and password='${encryptedPassword}'`)
+
+        if (result.length === 0) {
+          return res.status(403).json({message: "No user found"})
+        }
+
+        jwt.sign({id: result[0].id, email}, process.env.JWT_SECRET_KEY, {expiresIn: '1d'}, async (error, token) => {
+          if (error) {
+            return res.json({message: "Error when creating your token"})
+          } else {
+            return res.json({token, message: "Success"})
+          }
+        })
       }
     })
-
-    //return res.json({message: "Success token"})
   } catch (e) {
     return res.json({message: "Error:" + e.message})
   }
 
+})
+
+app.post('', async (req, res) => {
+
+  try {
+    res.setHeader("Content-Type", "application/json")
+    const {email, password, name} = req.body
+    if (!email) {
+      return res.status(400).json({message: "Email is not valid"})
+    }
+    if (!password || password.length < 8) {
+      return res.status(400).json({message: "Password is not valid"})
+    }
+    if (!name || name.length < 2) {
+      return res.status(400).json({message: "Name is not valid"})
+    }
+
+    let isEmailExist = await query(`select * from user where email='${email}'`);
+
+    if (isEmailExist.length > 0) {
+      return res.status(400).json({message: "Email is already exist!"})
+    }
+
+    jwt.sign(password, process.env.JWT_SECRET_KEY, {}, async (error, encryptedPassword) => {
+      if (error) {
+        return res.json({message: "Error when creating user"})
+      } else {
+        let insertQuery = `insert into user (email, password, name) values ('${email}','${encryptedPassword}','${name}')`
+        let insertResult = await query(insertQuery)
+        if (!insertResult.insertId) {
+          return res.status(400).json({message: "Error when creating user"})
+        }
+        return res.json({message: "Success"})
+      }
+    })
+
+  } catch (e) {
+    return res.json({message: "Error:" + e.message})
+  }
 })
 
 
